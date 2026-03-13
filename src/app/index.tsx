2441ffac01542eb6591fa5e7149cde9d9eef958b
Maria Eduarda Montecino <eduardamontecino@gmail.com>
2026-03-13 10:54:33 -0300

    :tada: Inicio do projeto

---
+++ src/app/index.tsx
@@ -0,0 +1,100 @@
+import { useEffect, useState } from "react";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+
+interface Pokemon {
+  name: string;
+  image: string;
+  imageShiny: string;
+  types: PokemonType;
+}
+
+interface PokemonType {
+  type: {
+    name: string;
+    url: string;
+  };
+}
+
+const colorsByType = {
+  bug: "#475c4c",
+  dark: "#50475c",
+  dragon: "#95bab8",
+  eletric: "#dbce8c",
+  fairy: "#ed9dc5",
+  fighting: "#f29988",
+  fire: "#f28888",
+  flying: "#90aed6",
+  ghost: "#8579ab",
+  grass: "#88ab79",
+  ground: "#705d55",
+  ice: "#8da6a5",
+  normal: "#e39da8",
+  poison: "#68567d",
+  psychic: "#ba7799",
+  rock: "#453834",
+  steel: "#5c5a59",
+  water: "#8689e3",
+};
+
+export default function Index() {
+  const [pokemons, setPokemon] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
+
+  async function fetchPokemons() {
+    try {
+      const reponse = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
+      const data = await reponse.json();
+
+      const detailedPokemons = await Promise.all(
+        data.results.map(async (pokemons: any) => {
+          const res = await fetch(pokemons.url);
+          const details = await res.json();
+          return {
+            name: pokemons.name,
+            image: details.sprites.front_default,
+            imageShiny: details.sprites.front_shiny,
+            types: details.types,
+          };
+        }),
+      );
+
+      setPokemon(detailedPokemons);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return (
+    <ScrollView>
+      {pokemons.map((pokemon) => (
+        <View key={pokemon.name} style={{ backgroundColor: "blue" }}>
+          <Text style={styles.name}>{pokemon.name}</Text>
+          <Text style={styles.type}>{pokemon.types[0].type.name}</Text>
+          <View style={{ flexDirection: "row" }}>
+            <Image source={{ uri: pokemon.image }} style={styles.image} />
+            <Image source={{ uri: pokemon.imageShiny }} style={styles.image} />
+          </View>
+        </View>
+      ))}
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 150,
+    height: 150,
+  },
+  name: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  type: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#5a5a5a",
+  },
+});
